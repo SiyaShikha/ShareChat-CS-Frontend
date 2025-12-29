9094c83ddac7d02b691fa153ab22cb3432582df3
Shikha Singh <siya@Shikhas-MacBook-Pro.local>
2025-12-29 17:19:39 +0530

    adds log for debugging

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+console.log("ENV CHECK:", import.meta.env);
+console.log("API URL CHECK:", import.meta.env.VITE_API_URL);
+
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <App />
